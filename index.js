c00d22db997e399a1e862639fad9a9e8918a574c
tothm23 <tothm.113sz@acsjszki.hu>
2023-03-18 21:23:45 +0100

    adding "0" if clock have one digit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const text = document.getElementById("clock");
 
 window.setInterval(() => {
-  const date = new Date();
-  text.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const now = new Date();
+  text.innerHTML = `
+  ${now.getHours().toString().padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}:${now.getSeconds().toString().padStart(2, "0")}
+`;
 }, 1000);
